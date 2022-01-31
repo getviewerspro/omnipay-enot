95dffa4f9507c5086f1d2cfe9bf054f31e4cfdc2
getviewerspro <46578299+getviewerspro@users.noreply.github.com>
2022-01-31 18:08:58 +0300

    Update PurchaseResponse.php

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -37,6 +37,7 @@ class PurchaseResponse extends AbstractResponse implements RedirectResponseInter
 
         if (!empty($this->data['paymentMethod']) AND $this->data['paymentMethod'] == 'qw') {
             $redirect = $this->_redirect_qiwi;
+            
             unset($this->data['paymentMethod']);
         }
 
